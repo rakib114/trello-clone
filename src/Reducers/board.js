5dcef5f9bf80e6977f749b7181ec5811648ec5e0
rakib114 <mahmud17209@gmail.com>
2022-06-07 10:41:17 +0600

    board reducer function

--- src/Reducers/board.js
+++ src/Reducers/board.js
@@ -1,10 +1,10 @@
-const boardReducer = (boards = [], action) => {
+export const boardReducer = (boards = [], action) => {
   switch (action.type) {
     case "CREATE_BOARD": {
       const board = {
         id: Date.now(),
         title: action.payload.title,
-        taskList: [],
+        taskLists: [],
         tasks: [],
         caretedAt: new Date().toLocaleDateString(),
       };
@@ -23,6 +23,46 @@ const boardReducer = (boards = [], action) => {
     }
     case "DELETE_BOARD": {
       return boards.filter((board) => board.id !== action.payload.id);
+    }
+
+    case "ADD_LIST_ID_TO_BOARD": {
+      return boards.map((board) => {
+        if (board.id === action.payload.id) {
+          board.taskLists.push(action.payload.taskList.id);
+        }
+        return board;
+      });
+    }
+
+    case "REMOVE_LIST_ID_FROM_BOARD": {
+      return boards.map((board) => {
+        if (board.id === action.payload.id) {
+          board.taskLists = board.taskLists.filter(
+            (item) => item !== action.payload.taskList.id
+          );
+        }
+        return board;
+      });
+    }
+
+    case "ADD_TASK_ID_TO_BOARD": {
+      return boards.map((board) => {
+        if (board.id === action.payload.id) {
+          board.tasks.push(action.payload.task.id);
+        }
+        return board;
+      });
+    }
+
+    case "REMOVE_TASK_ID_FROM_BOARD": {
+      return boards.map((board) => {
+        if (board.id === action.payload.id) {
+          board.tasks = board.tasks.filter(
+            (item) => item !== action.payload.task.id
+          );
+        }
+        return board;
+      });
     }
 
     default: {
